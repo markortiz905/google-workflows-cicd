6eca1bc682aa828486f618e796c97250fa20721a
Ghost Rider <mortiz@scentregroup.com>
2022-01-28 22:19:25 +0800

    samples

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@ echo "Authenticate gcloud..."
 gcloud auth activate-service-account --key-file=$GOOGLE_CLOUD_CREDENTIALS
 gcloud config set project $GCP_PROJECT
 
-
 deloy_workflows() {
+    echo "Apply secrets..."
+    sed -i -e 's/API_KEY_SECRET/$API_KEY/g' $1
     echo "Deploying [$3][$1] workflows to [$2]"
     gcloud workflows deploy $1 --location=asia-southeast1 --source=$3
 }
-
 
 for FILE in workflows/*; 
     do deloy_workflows $(basename $FILE|sed 's/.yaml//' ) $GCP_PROJECT $FILE
