abac4c7de25a59a5bcc2d62e45f67242708613e1
Ghost Rider <mortiz@scentregroup.com>
2022-01-28 22:35:40 +0800

    samples

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,11 @@ deloy_workflows() {
     echo "Apply secrets..."
     sed -i -e "s/API_KEY_SECRET/$API_KEY/g" $3
     echo "Deploying [$3][$1] workflows to [$2]"
-    gcloud workflows deploy $1 --location=asia-southeast1 --source=$3
+    gcloud workflows deploy $1 --location=asia-southeast1 --source=$3 >/dev/null 2>&1
+    echo "Deployed [$3][$1] workflows to [$2]"
 }
 
 for FILE in workflows/*; 
     do deloy_workflows $(basename $FILE|sed 's/.yaml//' ) $GCP_PROJECT $FILE
 done
-
-echo "API_KEY: $API_KEY"
 
